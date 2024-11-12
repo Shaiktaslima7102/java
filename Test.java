adbde2d92b5f68e5528741db5b4f2524eaccc271
ShaikTaslima <shaiktaslima710@gmail.com>
2024-11-12 22:54:49 +0530

    Updated Test.java

--- Test.java
+++ Test.java
@@ -4,10 +4,8 @@ public class Test {
     {
         int a=5;
         int b=10;
-        int c=18;
-        int d=21;
-        int e =a+b;
-        int res=a+b+c+d+e;
+        
+        int res=a+b;
         System.out.println("the result is " +res);
     }
     public static void main(String[] args)
